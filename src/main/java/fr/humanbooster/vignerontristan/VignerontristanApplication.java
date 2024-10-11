c0c6e71c40e251063881f22f69e80a3ef3624ac1
Arkalithe <arkalithe@gmail.com>
2024-10-11 09:43:12 +0200

    first commit

---
+++ src/main/java/fr/humanbooster/vignerontristan/VignerontristanApplication.java
@@ -0,0 +1,13 @@
+package fr.humanbooster.vignerontristan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VignerontristanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VignerontristanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/humanbooster/vignerontristan/VignerontristanApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.humanbooster.vignerontristan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VignerontristanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
